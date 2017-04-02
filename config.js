63f357a7a5c482a0bb6d46a6b6e81a51749b8510
weezyval <weezyval93@gmail.com>
2017-04-02 15:33:40 +0100

    First Commit

---
+++ config.js
@@ -0,0 +1,21 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'afrotonez',
+		wp_ws_url : 'https://afrotonez.com/wp-appkit-api/afrotonez',
+		wp_url : 'https://afrotonez.com',
+		theme : 'q-android',
+		version : '1.0.0',
+		app_title : 'afrotonez',
+		app_platform : 'android',
+		gmt_offset : 0,
+		debug_mode : 'off',
+		auth_key : 'bqrZ4D.>_.?c/cXu^R{_X!UE,Ix3m497GDBm+$dLI3V5lXh?fjq?h<)Xb|2A*=~r',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
